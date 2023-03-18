b5fafe3b1c252a0ea9cba3d10e5a429351a3c79e
GodYT88 <117300772+GodYT88@users.noreply.github.com>
2023-03-19 05:24:40 +0700

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,31 +1,39 @@
 if not _G.A then
-    _G.A = true
-_G.TESTOLDP = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+game:GetService("Players").LocalPlayer.PlayerGui.stamina.LocalScript.energy.Value = math.huge
+game:GetService("Players").LocalPlayer.PlayerGui.hiding.Heartbeat:Destroy()
+local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+    if v.Name == "locker" or v.Name == "locker 1" or v.Name == "locker 2" then
+        if not v.Seat:FindFirstChild("SeatWeld") then
+            local distance = (v.Seat.Position - HumanoidRootPart.Position).Magnitude
+                    if distance < 5 then
+                if HumanoidRootPart.Parent.safe.Value == 1 then
+                    v.Seat.Disabled = true
+                    wait()
+                    v.Seat.Disabled = false
+                    _G.A = true
+                    game.StarterGui:SetCore("SendNotification", {
+Title = "GodY Hub"; -- the title (ofc)
+Text = "Done!"; -- what the text says (ofc)
+Duration = 5; -- how long the notification should in secounds
+})
+elseif HumanoidRootPart.Parent.safe.Value == 0 then
+    break
+                end
+            end
+        end
+    end
+end
 game:GetService("Workspace").monster.hunt.Changed:connect(function(S)
-    local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
     if S == true then
         game.StarterGui:SetCore("SendNotification", {
 Title = "GodY Hub"; -- the title (ofc)
 Text = "A-60 Start Hunt"; -- what the text says (ofc)
 Duration = 5; -- how long the notification should in secounds
 })
-        _G.TESTOLDP = HumanoidRootPart.CFrame
-        repeat wait()
-for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-    if v.Name == "locker" or v.Name == "locker 1" or v.Name == "locker 2" then
-        if not v.Seat:FindFirstChild("SeatWeld") then
-            local distance = (v.Seat.Position - HumanoidRootPart.Position).Magnitude
-                    if distance < 500 then
-                HumanoidRootPart.CFrame = v.Seat.CFrame
-            end
-        end
-    end
-end
-until HumanoidRootPart.Parent.safe.Value == 1
+
 elseif S == false then
-    HumanoidRootPart.Parent.Humanoid.Sit = false
-    wait()
-    HumanoidRootPart.CFrame = _G.TESTOLDP
+
     game.StarterGui:SetCore("SendNotification", {
 Title = "GodY Hub"; -- the title (ofc)
 Text = "A-60 Stop Hunt"; -- what the text says (ofc)
@@ -35,30 +43,15 @@ end
 end)
 
 game:GetService("Workspace").monster2.hunt.Changed:connect(function(S)
-    local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
     if S == true then
         game.StarterGui:SetCore("SendNotification", {
 Title = "GodY Hub"; -- the title (ofc)
 Text = "Unknow Start Hunt"; -- what the text says (ofc)
 Duration = 5; -- how long the notification should in secounds
 })
-        _G.TESTOLDP = HumanoidRootPart.CFrame
-        repeat wait()
-for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-    if v.Name == "locker" or v.Name == "locker 1" or v.Name == "locker 2" then
-        if not v.Seat:FindFirstChild("SeatWeld") then
-            local distance = (v.Seat.Position - HumanoidRootPart.Position).Magnitude
-                    if distance < 500 then
-                HumanoidRootPart.CFrame = v.Seat.CFrame
-            end
-        end
-    end
-end
-until HumanoidRootPart.Parent.safe.Value == 1
+
 elseif S == false then
-    HumanoidRootPart.Parent.Humanoid.Sit = false
-    wait()
-    HumanoidRootPart.CFrame = _G.TESTOLDP
+
     game.StarterGui:SetCore("SendNotification", {
 Title = "GodY Hub"; -- the title (ofc)
 Text = "Unknow Stop Hunt"; -- what the text says (ofc)

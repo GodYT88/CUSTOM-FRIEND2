ac905bb4bd7b634e2dfd7f3e46f79f4782660a2f
GodYT88 <117300772+GodYT88@users.noreply.github.com>
2023-03-19 09:44:39 +0700

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,27 +1,12 @@
-setclipboard("_G.Multi = 50")
-
-local old; old = hookmetamethod(game, '__namecall', function(self, ...)
-  local args = {...}
-  local method = getnamecallmethod()
-
-  if not checkcaller() and method == 'InvokeServer' and self.Name == "StrongMan_UpgradeStrength" and args[2] == "Default" then
-            args[1] = _G.Multi
-        end
-
-  return old(self, unpack(args))
-end)
-
-while wait() do
-    pcall(function()
-    game:GetService("Workspace").Areas.Area1.Exit.Goal.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    game:GetService("Workspace").Areas.Area1.Exit.Goal.Size = Vector3.new(9e9,9e9,9e9)
-    end)
+local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+    if v.Name == "Hidden" then
+        local distance = (v.Position - HumanoidRootPart.Position).Magnitude
+            if distance < 5 then
+                v.Disabled = false
+                wait()
+                v.Disabled = true
+                v.Disabled = false
+            end
+    end
 end
-
-
-local args = {
-    [1] = 1,
-    [2] = "Lightning"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("StrongMan_UpgradeStrength"):InvokeServer(unpack(args))

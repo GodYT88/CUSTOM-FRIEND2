d1f67752b66f7ef69aeed932eecbe107888097a8
GodYT88 <117300772+GodYT88@users.noreply.github.com>
2023-03-19 10:14:22 +0700

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,12 +1,2 @@
-local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
-for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-    if v.Name == "Hidden" then
-        local distance = (v.Position - HumanoidRootPart.Position).Magnitude
-            if distance < 5 then
-                v.Disabled = false
-                wait()
-                v.Disabled = true
-                v.Disabled = false
-            end
-    end
-end
+game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.Full:Destroy()
+game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.DeadHint:Destroy()

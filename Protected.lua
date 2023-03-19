0601c3be26a78027a12bfcaa03df253c18b8db3a
GodYT88 <117300772+GodYT88@users.noreply.github.com>
2023-03-19 08:29:07 +0700

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -10,3 +10,10 @@ local old; old = hookmetamethod(game, '__namecall', function(self, ...)
 
   return old(self, unpack(args))
 end)
+
+while wait() do
+    pcall(function()
+    game:GetService("Workspace").Areas.Area1.Exit.Goal.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    game:GetService("Workspace").Areas.Area1.Exit.Goal.Size = Vector3.new(9e9,9e9,9e9)
+    end)
+end

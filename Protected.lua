87f39198176f0b5301cd7954beeaf96dcd7a8c55
GodYT88 <117300772+GodYT88@users.noreply.github.com>
2023-03-19 08:13:32 +0700

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,17 +1,6 @@
-game.Workspace.ChildAdded:Connect(function(model)
-    if model.Name == "GrabParts" then
-        local part_to_impulse = model["GrabPart"]["WeldConstraint"].Part1
- 
-        if part_to_impulse then
-            
-            if part_to_impulse.Parent:FindFirstChildOfClass("Humanoid") then
- 
-                local head = part_to_impulse.Parent["Head"]
-                local a = Instance.new("BodyForce" , head.Parent.HumanoidRootPart)
-                a.Force = Vector3.new(2500, 2500, -7500)
-                wait(3)
-                a:Destroy()
-            end
-        end
-    end
-end)
+while wait() do
+    pcall(function()
+    game:GetService("Workspace").Areas.Area1.Exit.Goal.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    game:GetService("Workspace").Areas.Area1.Exit.Goal.Size = Vector3.new(9e9,9e9,9e9)
+    end)
+end
